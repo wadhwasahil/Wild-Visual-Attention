27263706db4a3c90fd38a0ac7c015895b80083a7
wadhwasahil <sahil24wadhwa@gmail.com>
2017-03-19 22:47:36 +0530

    Add padding for conv and pool layers

--- controller.py
+++ controller.py
@@ -1,10 +1,17 @@
 import read_data
 import tensorflow as tf
 import numpy as np
-
+import spatial_glimpse_network
 
 data = read_data.read()
 
 img = next(data)
-img = np.resize(img, (32, 32, 3))
-print(img.shape)
+img = np.expand_dims(np.resize(img, (32, 32, 3)), 0)
+with tf.Graph().as_default():
+    sess = tf.Session()
+    with sess.as_default():
+        cnn = spatial_glimpse_network.SGN()
+        sess.run(tf.global_variables_initializer())
+        pool = sess.run([cnn.pool], feed_dict={cnn.input_x:img})
+
+
